70e12f4d3c32616fe80550d120139f8f8ff6e249
ctninman <89664157+ctninman@users.noreply.github.com>
2022-09-19 10:18:40 -0400

    find average pair challenge

--- challenges.js
+++ challenges.js
@@ -41,7 +41,57 @@ function areThereDuplicates() {
 	return false
 }
 
-console.log(areThereDuplicates(1,2,3))
-console.log(areThereDuplicates(1,2,3,1))
-console.log(areThereDuplicates(1,2,3,'w','2'))
-console.log(areThereDuplicates(1))
+// console.log(areThereDuplicates(1,2,3))
+// console.log(areThereDuplicates(1,2,3,1))
+// console.log(areThereDuplicates(1,2,3,'w','2'))
+// console.log(areThereDuplicates(1))
+
+
+// MULTIPLE POINTERS AVERAGE PAIR CHALLENGE
+// given sorted array and target average, determine if there is a pair of numbers who have average of target
+// return true or false
+
+function averagePair (arr, targetAvg) {
+	let firstIndex = 0;
+	let lastIndex = arr.length - 1;
+	let middle = Math.floor(arr.length / 2)
+		if (targetAvg > arr[middle]) {
+			console.log('1');
+			firstIndex = middle + 1;
+		} else if (targetAvg < arr[middle]) {
+			console.log('2');
+			lastIndex = middle - 1;
+		}
+		
+	if ((arr[middle] + arr[middle + 1]) / 2 === targetAvg){
+		console.log('3');
+		return true
+	} else if ((arr[middle] + arr[middle - 1]) / 2 === targetAvg){
+		console.log('4');
+		return true
+	}
+	return false
+}
+
+console.log(averagePair([1,2,4,5,6,7,8,9,11,12,13,14], 12.5));
+
+function yoAverage (arr, targetAvg) {
+	let start = 0;
+	let end = arr.length - 1;
+	let avg = (arr[start] + arr[end]) / 2
+	while (start < end) {
+		avg = (arr[start] + arr[end]) / 2
+		console.log('avg = ', avg, 'targ = ', targetAvg);
+		console.log(arr[start], arr[end])
+		if (avg === targetAvg) {
+			return true;
+		} else if (avg < targetAvg) {
+			start++;
+		} else {
+			end--;
+		}
+	}
+	return false;
+}
+
+console.log(yoAverage([1,2,4,5,6], 4.5));
