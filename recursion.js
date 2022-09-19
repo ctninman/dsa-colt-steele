93249192cf23a6937fb34eb13794b74a41dec1f6
ctninman <89664157+ctninman@users.noreply.github.com>
2022-09-19 12:29:15 -0400

    recursion

--- recursion.js
+++ recursion.js
@@ -26,3 +26,24 @@ function recursiveCountDown (num) {
 }
 
 console.log(recursiveCountDown(10))
+
+function sumRange (num) {
+	if (num === 1) return 1;
+	return num + sumRange(num - 1)
+}
+
+function nonRecursiveFactorial (num) {
+	let total = 1;
+	for (let i = num; i > 1; i--) {
+		total *= i
+	}
+	return total;
+}
+
+function recursiveFactorial (num) {
+  if (num === 1) return 1
+	return num * recursiveFactorial(num - 1)
+}
+
+console.log(nonRecursiveFactorial(10))
+console.log(recursiveFactorial(10))
