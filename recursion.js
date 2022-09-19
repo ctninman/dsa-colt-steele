85c72b1ca56800a8a5be4c3bfd2d72d589659cf5
ctninman <89664157+ctninman@users.noreply.github.com>
2022-09-19 13:42:47 -0400

    recursion challenges

--- recursion.js
+++ recursion.js
@@ -25,7 +25,7 @@ function recursiveCountDown (num) {
 	recursiveCountDown(num)
 }
 
-console.log(recursiveCountDown(10))
+// console.log(recursiveCountDown(10))
 
 function sumRange (num) {
 	if (num === 1) return 1;
@@ -45,5 +45,55 @@ function recursiveFactorial (num) {
 	return num * recursiveFactorial(num - 1)
 }
 
-console.log(nonRecursiveFactorial(10))
-console.log(recursiveFactorial(10))
+// console.log(nonRecursiveFactorial(10))
+// console.log(recursiveFactorial(10))
+
+
+// HELPER METHOD RECURSION
+
+function collectOddValues (arr) {
+	let result = [];
+
+	function helper(helperInput) {
+		if (helperInput.length <= 0) return;
+		if(helperInput[0] % 2 !== 0) {
+			result.push(helperInput[0]);
+		}
+		helper(helperInput.slice(1));
+	}
+	helper(arr);
+	return result;
+}
+
+function collectOddsPureRecursion (arr) {
+	let newArr = [];
+
+	if (arr.length === 0) return newArr;
+
+	if (arr[0] % 2 !== 0) {
+		newArr.push(arr[0]);
+	}
+
+	newArr = newArr.concat(collectOddsPureRecursion(arr.slice(1)));
+	return newArr;
+}
+
+// console.log(collectOddValues([1,2,3,4,5,5,7,9,8,9]))
+// console.log(collectOddsPureRecursion([1,2,3,4,5,5,7,9,8,9]))
+
+// console.log(5 ** 4);
+
+// Recursive function which takes array of numbers and returns the product of them all
+function productOfArray (arr) {
+	if (arr.length === 0) return 0;
+	if (arr.length === 1) return arr[0];
+	return arr[arr.length - 1] * productOfArray(arr.slice(0,-1))
+}
+
+// Recursive function that add up all numbers from 0 to the passed in number
+function recursiveRange (num) {
+	if (num === 1) return 1;
+	return num + recursiveRange(num -1)
+}
+
+console.log(recursiveRange(10))
