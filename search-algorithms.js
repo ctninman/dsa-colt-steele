eb361796d93a5d5d5d4d33c3cdd5d1f877185729
ctninman <89664157+ctninman@users.noreply.github.com>
2022-09-20 12:47:17 -0400

    compare to colt binary solutions

--- search-algorithms.js
+++ search-algorithms.js
@@ -46,3 +46,33 @@ function homemadeBinarySearch (arr, val) {
 	}
 	return -1
 }
+
+function coltHomemadeBinary (arr, val) {
+	let start = 0;
+	let end = arr.length - 1;
+	let middle = Math.floor((start + end) / 2);
+	while (arr[middle] !== val && start <= end) {
+		if (val < arr[middle]) {
+			end = middle - 1;
+		} else {
+			start = middle + 1;
+		}
+		middle = Math.floor((start + end) / 2);
+	}
+	if (arr[val] === val) {
+		return middle;
+	} 
+	return -1;
+}
+
+function coltHomemadeBinaryFewerLines (arr, val) {
+	let start = 0;
+	let end = arr.length - 1;
+	let middle = Math.floor((start + end) / 2);
+	while (arr[middle] !== val && start <= end) {
+		if (val < arr[middle]) end = middle - 1;
+		else start = middle + 1;
+		middle = Math.floor((start + end) / 2);
+	}
+	return arr[middle] === val ? middle : -1
+}
