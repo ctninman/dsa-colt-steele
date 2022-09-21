f9a232569d380ab01c084c5b6e57b0a4b436334e
ctninman <89664157+ctninman@users.noreply.github.com>
2022-09-21 12:35:33 -0400

    merge function

---
+++ merge-sort.js
@@ -0,0 +1,41 @@
+// MERGE SORT
+
+/*
+	O(n log n)
+
+	combination of splitting, sorting, and merging
+	takes advantage that arrays with length 0 or 1 are already sorted
+	works by decomposing an array into smaller arrays of 0 or 1 elements, then building
+		up a newly sorted array
+
+*/
+
+/*
+
+	Writing a function to merge to sorted arrays
+
+*/
+
+function merge (arr1, arr2) {
+	let results = [];
+	let arr1index = 0;
+	let arr2index = 0;
+	while (arr1index < arr1.length && arr2index < arr2.length) {
+		if (arr2[j] > arr1[i]) {
+			results.push(arr1[i]);
+			i++;
+		} else {
+			results.push(arr2[j]);
+			j++;
+		}
+		while (arr1index < arr1.length) {
+			results.push(arr1[i]);
+			i++
+		}
+		while (arr2index < arr2.length) {
+			results.push(arr2[j]);
+			j++;
+		}
+	}
+	return results;
+}
