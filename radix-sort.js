1c8bbffb16a584999680e64c761c92e1170ce7a9
ctninman <89664157+ctninman@users.noreply.github.com>
2022-09-27 11:57:46 -0400

    added singly linked list push

--- radix-sort.js
+++ radix-sort.js
@@ -21,3 +21,14 @@ function mostDigits (nums) {
 	}
 	return maxDigits;
 }
+
+
+function radixSort (nums) {
+	let maxDigitCount = mostDigits(nums);
+	for (let k = 0; k < maxDigitCount; k++) {
+		let digitBuckets = Array.from({length: 10}, () => [])
+		for (let i = 0; i < nums.length; i++) {
+			nums[i]
+		}
+	}
+}
