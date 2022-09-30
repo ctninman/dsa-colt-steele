106e872601e57a0f0b93512e52a21cd8067fbb69
ctninman <89664157+ctninman@users.noreply.github.com>
2022-09-29 21:10:53 -0400

    created stack with linked list

--- stacks.js
+++ stacks.js
@@ -28,3 +28,42 @@ stack.pop();
 
 // Stack implementation with a linked list
 
+class Node {
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+	}
+}
+
+class Stack {
+	constructor() {
+		this.first = null;
+		this.last = null;
+		this.size = 0;
+	}
+
+	firstIn (val) {
+		let newNode = new Node(val);
+		if (this.length === 0) {
+			this.first = newNode;
+			this.last = newNode;
+		} else {
+			let temp = this.first;
+			this.head = newNode;
+			newNode.next = temp;
+		}
+		return ++this.size;
+	}
+
+	firstOut () {
+		if (!this.first) return null;
+		let temp = this.first;
+		if (this.first === this.last) {
+			this.last = null;
+		}
+		this.first = this.first.next;
+		this.size--;
+		return temp.value;
+	}
+
+}
