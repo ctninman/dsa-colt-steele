a4c47f245402a95eb956eea8451e6ce9b135a9f6
ctninman <89664157+ctninman@users.noreply.github.com>
2022-09-29 20:59:36 -0400

    created stack with array

---
+++ stacks.js
@@ -0,0 +1,30 @@
+// STACKS
+
+// LIFO
+// Last in first out
+// Pringles
+
+// Think about the call stack
+// Undo/ Redo
+// Managing function invocations
+// Routing (the history objects)
+
+// Creating a stack with an array
+
+let stack = [];
+
+stack.push('google')
+stack.push('instagram')
+stack.push('youtube')
+
+stack.pop();
+// youtube
+stack.pop();
+// instagram
+stack.pop();
+// google
+
+// shift and unshift also act as a stack
+
+// Stack implementation with a linked list
+
