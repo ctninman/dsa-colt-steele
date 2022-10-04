c5aacc3323ba798db8485dbf893a2f36c921c2e9
ctninman <89664157+ctninman@users.noreply.github.com>
2022-10-04 14:17:54 -0400

    added heap extractMax

--- heaps.js
+++ heaps.js
@@ -27,7 +27,51 @@ class MaxBinaryHeap {
 			this.values[idx] = parent;
 			idx = parentIdx;
 		}
-		console.log(this.values)
+	}
+
+	extractMax () {
+		const max = this.values[0];
+		const lastElement = this.values.pop();
+		
+		if (this.values.length > 0) {
+			this.values[0] = lastElement;
+			this.sinkDown();
+		}
+		
+		return max;
+	}
+
+	sinkDown () {
+		let idx = 0;
+		const length = this.values.length;
+		const element = this.values[0];
+
+		while (true) {
+			let leftChildIdx = 2 * idx + 1;
+			let rightChildIdx = 2 * idx + 2;
+			let leftChild, rightChild;
+			let swap = null;
+
+			if (leftChildIdx < length) {
+				leftChild = this.values[leftChildIdx];
+				if (leftChild > element) {
+					swap = leftChildIdx
+				}
+ 			}
+			if (rightChildIdx < length) {
+				rightChild = this.values[rightChildIdx];
+				if (
+					(swap === null && rightChild > element) ||
+					(swap !== null && rightChild > leftChild) 
+				) {
+					swap = rightChildIdx;
+				}
+			}
+			if (swap === null) break;
+			this.values[idx] = this.values[swap];
+			this.values[swap] = element;
+			idx = swap;
+		}
 	}
 }
 
