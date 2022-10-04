a0f753c5e72542fb2fa49a63b794263980bac35a
ctninman <89664157+ctninman@users.noreply.github.com>
2022-10-03 23:29:34 -0400

    added breadth first search

--- tree-traversals.js
+++ tree-traversals.js
@@ -71,6 +71,21 @@ For any type of tree, not just binary search trees
 			}
 			return false;
 		}
+
+		BFS () {
+			let data = [],
+					queue = [],
+					node = this.root;
+			queue.push(this.root);
+			// while there is something in the queue
+			while(queue.length) {
+				node = queue.shift();
+				data.push(node);
+				if(node.left) queue.push(node.left);
+				if(node.right) queue.push(node.right);
+			}
+			return data;
+		}
 	
 	}
 	
@@ -80,3 +95,13 @@ For any type of tree, not just binary search trees
 	tree.root.right = new Node(15);
 	tree.root.left = new Node(7);
 	tree.root.left.right = new Node(9);
+
+	// Breadth-first, check all siblings before children
+
+// Create a queue, (can use array), and a variable to store the values of nodes visited
+// Place the root node in the queue
+// Loop as long as there is anything in the queue
+//		Dequeue a node from the queue and push the value of the node into the variable that stores the nodes
+//    If there is a left property on the node dequeued - add it to the queue
+//		If there is a right property on the node dequeued - add it to the queue
+
