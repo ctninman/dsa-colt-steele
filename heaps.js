33688e9e021c402cab650aff9fad711a4fdb6355
ctninman <89664157+ctninman@users.noreply.github.com>
2022-10-04 15:05:28 -0400

    added priority queue

--- heaps.js
+++ heaps.js
@@ -37,7 +37,7 @@ class MaxBinaryHeap {
 			this.values[0] = lastElement;
 			this.sinkDown();
 		}
-		
+
 		return max;
 	}
 
@@ -89,3 +89,87 @@ newHeap.insert(85)
 newHeap.insert(75)
 newHeap.insert(45)
 newHeap.insert(95)
+
+class PQNode {
+	constructor (val, priority) {
+		this.val = val;
+		this.priority = priority;
+	}
+}
+class PriorityQueue {
+	constructor () {
+		this.values = [];
+	}
+
+	enqueue (val, priority) {
+		let newNode = new PQNode(val,priority)
+		this.values.push(newNode);
+
+		let idx = this.values.length - 1;
+		const element = this.values[idx];
+		
+		while (idx > 0) {
+			let parentIdx = Math.floor((idx - 1) / 2)
+			let parent = this.values[parentIdx];
+
+			if (element.priority >= parent.priority) break;
+			
+			this.values[parentIdx] = element;
+			this.values[idx] = parent;
+			idx = parentIdx;
+		}
+	}
+
+	dequeue () {
+		const min = this.values[0];
+		const lastElement = this.values.pop();
+		
+		if (this.values.length > 0) {
+			this.values[0] = lastElement;
+			this.sinkDown();
+		}
+
+		return min;
+	}
+
+	sinkDown () {
+		let idx = 0;
+		const length = this.values.length;
+		const element = this.values[0];
+
+		while (true) {
+			let leftChildIdx = 2 * idx + 1;
+			let rightChildIdx = 2 * idx + 2;
+			let leftChild, rightChild;
+			let swap = null;
+
+			if (leftChildIdx < length) {
+				leftChild = this.values[leftChildIdx];
+				if (leftChild.priority < element.priority) {
+					swap = leftChildIdx
+				}
+ 			}
+			if (rightChildIdx < length) {
+				rightChild = this.values[rightChildIdx];
+				if (
+					(swap === null && rightChild.priority < element.priority) ||
+					(swap !== null && rightChild.priority < leftChild.priority) 
+				) {
+					swap = rightChildIdx;
+				}
+			}
+			if (swap === null) break;
+			this.values[idx] = this.values[swap];
+			this.values[swap] = element;
+			idx = swap;
+		}
+	}
+}
+
+let ER = new PriorityQueue ();
+
+ER.enqueue('common cold', 5)
+ER.enqueue('head wound', 1)
+ER.enqueue('high fever', 4)
+ER.enqueue('broken arm', 2)
+ER.enqueue('gash', 3)
